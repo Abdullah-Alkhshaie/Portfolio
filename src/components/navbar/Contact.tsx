dad3bf7d8753d0f7918daa387bf835631157971b
abdullah-alkhshaie <abdullahalkhshaie@gmail.com>
2024-02-04 17:58:21 +0300

    add changes

--- src/components/navbar/Contact.tsx
+++ src/components/navbar/Contact.tsx
@@ -5,22 +5,37 @@ import { MdEmail } from "react-icons/md";
 function Contact() {
   return (
     <div className="flex gap-3 items-center">
-      <MdEmail
-        size={25}
-        className="hover:scale-125 sca duration-300 cursor-ponter cursor-pointer"
-      />
-      <FaXTwitter
-        size={23}
-        className="hover:scale-125 sca duration-300 cursor-ponter cursor-pointer"
-      />
-      <FaGithub
-        size={25}
-        className="hover:scale-125 sca duration-300 cursor-ponter cursor-pointer"
-      />
-      <FaLinkedin
-        size={25}
-        className="hover:scale-125 sca duration-300 cursor-ponter cursor-pointer"
-      />
+      <a
+        href="mailto:abdullahalkhshaie@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <MdEmail
+          size={25}
+          className="hover:scale-125 sca duration-300 cursor-ponter cursor-pointer"
+        />
+      </a>
+      <a target="_blank" href="https://twitter.com/Dev_Abdullah1">
+        <FaXTwitter
+          size={23}
+          className="hover:scale-125 sca duration-300 cursor-ponter cursor-pointer"
+        />
+      </a>
+      <a target="_blank" href="https://github.com/Abdullah-Alkhshaie">
+        <FaGithub
+          size={25}
+          className="hover:scale-125 sca duration-300 cursor-ponter cursor-pointer"
+        />
+      </a>
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/abdullah-alkhushei-837546266?originalSubdomain=ye"
+      >
+        <FaLinkedin
+          size={25}
+          className="hover:scale-125 sca duration-300 cursor-ponter cursor-pointer"
+        />
+      </a>
     </div>
   );
 }
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -21,58 +21,72 @@ function Projects() {
     {
       img: megaImg,
       name: "MegaShop",
+      github: "https://github.com/Abdullah-Alkhshaie/megashop-2",
+      site: "https://megashopstore.netlify.app/",
     },
     {
       img: movieImg,
-      name: "MegaShop",
+      name: "Netflix",
+      github: "https://github.com/Abdullah-Alkhshaie/Netflex",
+      site: "https://abdullah-alkhshaie.github.io/Netflex/",
     },
     {
       img: bitesImg,
-      name: "MegaShop",
+      name: "Bites",
+      github: "https://github.com/Abdullah-Alkhshaie/Bites-4",
+      site: "https://bites-4.pages.dev/",
     },
     {
       img: comfyImg,
-      name: "MegaShop",
+      name: "Comfy store",
+      github: "https://github.com/Abdullah-Alkhshaie/comfy",
+      site: "https://abdullah-alkhshaie.github.io/Comfy/",
     },
     {
       img: colorImg,
-      name: "MegaShop",
+      name: "Color Flipper",
+      github: "https://github.com/Abdullah-Alkhshaie/Color-Flipper",
+      site: "https://abdullah-alkhshaie.github.io/Color-Flipper/",
     },
     {
       img: stripeImg,
-      name: "MegaShop",
+      name: "Stripe Menu",
+      github: "https://github.com/Abdullah-Alkhshaie/Strape-menu",
+      site: "https://abdullah-alkhshaie.github.io/Strape-menu/",
     },
   ];
 
   return (
     <div className="py-10  px-2 md:px-10 h-full lg:h-[90vh]">
       <h1 className="text-xl md:text-3xl">{t("projectHead")}:</h1>
-      <div className="mt-3 grid gird-cols-1 md:grid-cols-2 gap-2">
+      <div className="mt-5 grid gird-cols-1 md:grid-cols-2 gap-2">
         {projectsData.map((product, i) => (
           <div
             onMouseOver={() => setActiveProduct(i)}
             onMouseLeave={() => setActiveProduct(null)}
             key={i}
-            className="relative"
+            className="relative rounded-lg "
           >
-            <img src={product.img} className="w-[500px]" alt={product.name} />
-            <div className="absolute top-0">
+            <img
+              src={product.img}
+              className="w-[500px] rounded-lg"
+              alt={product.name}
+            />
+            <div className="absolute top-0 rtl:left-0">
               <h1
-                className={`text-lg bold font-bold ${
+                className={`text-lg bold  duration-700 rounded-br-lg font-bold ${
                   theme === "dark"
                     ? "bg-darkMode-background"
                     : "bg-lightMode-background"
                 } p-1  ${
-                  activeProduct === i
-                    ? "w-full opacity-100   duration-700"
-                    : "w-[0px] opacity-0   "
+                  activeProduct === i ? "opacity-100  " : "opacity-0    "
                 } `}
               >
                 {product.name}
               </h1>
             </div>
-            <div className="flex absolute z-10  gap-5 px-2">
-              <a href="google.com">
+            <div className="flex absolute z-10  rtl:left-5 px-2">
+              <a target="_blank" href={product.site}>
                 <FaEye
                   className={`cursor-pointer absolute ${
                     theme === "dark"
@@ -80,13 +94,13 @@ function Projects() {
                       : "text-darkMode-text"
                   } ${
                     activeProduct === i
-                      ? "opacite-100 duration-300 bottom-3 "
+                      ? "opacity-100 duration-300 bottom-3 "
                       : "opacity-0 -bottom-2  -z-0"
                   } `}
                   size={20}
                 />
               </a>
-              <a href="google.com">
+              <a target="_blank" href={product.github}>
                 <TfiGithub
                   className={`cursor-pointer absolute ${
                     theme === "dark"
@@ -94,14 +108,14 @@ function Projects() {
                       : "text-darkMode-text"
                   } ${
                     activeProduct === i
-                      ? "opacite-100 duration-500 bottom-3 left-14 rtl:right-14 "
+                      ? "opacite-100 duration-500 bottom-3 left-10 rtl:left-5  "
                       : "opacity-0 -bottom-2  -z-0"
                   } `}
                   size={20}
                 />
               </a>
             </div>
-            <div className="w-full h-full absolute bg-black opacity-80 hover:opacity-20 duration-300 top-0"></div>
+            <div className="w-full h-full rounded-lg absolute bg-black opacity-80 hover:opacity-20 duration-300 top-0"></div>
           </div>
         ))}
       </div>
